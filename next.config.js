204f549f2495410b170ef2d3faca0ded4662d199
M-santo <marcelo_santo1@outlook.com>
2025-04-16 07:39:02 -0300

    KLN6

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-// next.config.js
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 module.exports = withPWA({
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development", // PWA será desativado em desenvolvimento
-  },
+  // outras configurações, se necessário
 });
